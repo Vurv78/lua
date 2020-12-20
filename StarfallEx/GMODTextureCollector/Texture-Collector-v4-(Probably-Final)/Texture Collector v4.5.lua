d9734945b3024039031ecd9cbe3c5df315d75965
Vurv <56230599+Vurv78@users.noreply.github.com>
2020-12-19 20:02:10 -0800

    Create Texture Collector v4.5.lua
    This version doesn't rely on the png library. It instead makes a compressed image file that only takes 3 bytes per rgb color. It also uses fastlz to compress the result which helps out a ton in the case of maps like bigcity that have very predictable textures with bland windows and stuff.

---
+++ StarfallEx/GMODTextureCollector/Texture-Collector-v4-(Probably-Final)/Texture Collector v4.5.lua
@@ -0,0 +1,100 @@
+--@name Texture Collector v4.5
+--@author Vurv
+--@client
+
+-- Texture Collector v4 by Vurv on Discord (363590853140152321)
+-- Allows you to customize the pixel saving / file saving much more easily with an example of a compressed texture, that is ~200kb per 512x512 texture.
+-- Takes ~80 seconds to save all of the textures in bigcity as these compressed 200kb text files. (~60MB)
+
+if player() ~= owner() then return end
+
+local CPUMax = 0.9 -- 0-1 as a percentage. Higher is more unstable without extra quota checks.
+local FilePath = "textures/@.txt" -- @ will be replaced with the texture name.
+
+local FindMatFunc = function(mat) -- Needs to return width and height
+    return 512,512
+end
+
+-- locals for optimization
+local render = render
+local readPixel = render.readPixel
+local format = string.format
+
+local PixelFunc = function(r,g,b) -- PNG Pixel saving function
+    return format("%c%c%c",r,g,b)
+end
+
+local SaveFunc = function(Pixels,Path) -- PNG File saving Function
+    local data = fastlz.compress( table.concat(Pixels) )
+    file.write(Path,data)
+end
+
+local Materials = {}
+
+-- init
+file.createDir("textures")
+render.createRenderTarget("rt")
+
+local function canRun()
+    return quotaTotalAverage()<quotaMax()*CPUMax
+end
+
+local function quotaCheck()
+    if not canRun() then coroutine.yield() end
+end
+
+local usemat = material.create("UnlitGeneric")
+usemat:setInt("$flags",0)
+
+local function main()
+    local Started = timer.systime()
+    local SurfaceInfo = find.byClass("worldspawn")[1]:getBrushSurfaces()
+    local MaterialsFound = 0
+    for K,V in pairs(SurfaceInfo) do
+        local N = V:getMaterial():getName() -- Locked materials fucking useless !!!! omg!!
+        quotaCheck()
+        if not Materials[N] then Materials[N] = true MaterialsFound = MaterialsFound + 1 end
+    end
+    print(Color(50,255,255),format("Successfully found [%d] materials!!",MaterialsFound))
+    print(Color(255,255,50),"Starting to load textures, look in console for more details")
+    for Name in next,Materials do
+        printMessage(2,"Loading mat"..Name.."\n")
+        local FixedName = string.replace(Name,"/","_") -- We have to replace /'s because they interfere with .vex file comments which are used as: // comment
+        local Path = string.replace(FilePath,"@",FixedName)
+        quotaCheck()
+        if file.exists(Path) then print(Color(255,50,50),"Failed to load mat "..Name..", it already exists!") continue end
+        usemat:setTexture("$basetexture",material.getTexture(Name,"$basetexture"))
+        render.setMaterial(usemat)
+        local Width,Height = FindMatFunc(usemat)
+        render.selectRenderTarget("rt")
+            render.drawTexturedRect(0,0,Width,Height)
+            render.capturePixels()
+        render.selectRenderTarget()
+        local Pixels = {}
+        for Y = 0,Height-1 do
+            for X = 0,Width-1 do
+                quotaCheck()
+                local C = readPixel(X,Y)
+                local V = PixelFunc(C.r,C.g,C.b)
+                if V then Pixels[X+Y*Height+1] = V end
+                quotaCheck()
+            end
+        end
+        SaveFunc(Pixels,Path)
+        print(Color(50,255,50),"Successfully saved file "..Path)
+        Pixels = nil
+        quotaCheck()
+    end
+    print("Finished in "..tostring(timer.systime()-Started).."s")
+    return true
+end
+
+co = coroutine.create(main)
+
+hook.add("renderoffscreen","",function()
+    if coroutine.status(co) ~= "dead" then
+        if canRun() then
+            coroutine.resume(co)
+        end
+    end
+end)

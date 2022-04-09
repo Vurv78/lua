95593626548cb41eade9765b62712b3d47fd8818
Vurv <56230599+Vurv78@users.noreply.github.com>
2022-04-08 20:08:33 -0700

    Update RT-Camera.lua
    Heavy optimizations
    * Remove hooks when not rendering
    * Remove a lot of runtime var checking
    * Add ZeroUs config to not render the "Press E to Enable" when disabled

--- StarfallEx/RT-Camera.lua
+++ StarfallEx/RT-Camera.lua
@@ -3,19 +3,25 @@
 --@client
 --@model models/dav0r/camera.mdl
 
--- Author -- Vurv
--- Source -- https://github.com/Vurv78/starfallex-creations
--- Purpose -- This is a starfall chip that works just like an RT camera, except you can configure the resolution, field of view/zooming, frames per second and more.
--- Tbh, It feels less laggier than actual RT Cameras and has better quality. You can have multiple of these placed down, they will share cpu but not really well after the second camera.
+--[[
+    Source: https://github.com/Vurv78/lua
+    Desc:
+        Better quality and higher FPS RT Camera than actual addons, even with the overhead that SF has.
 
--- Disabled by default to prevent lag
+        Disabled for anyone but owner by default to prevent lag.
+        Although this only uses ~500us even with 60fps 1024x1024.
+        
+        Hyperoptimized, most of these optimizations are unrealistic but if you want to see how far you can optimize an SF chip,
+        here's your example.
+]]
 
 -- Configs --
-local FPS = 60                          -- Frames per second
+local FPS = 60
 local FOV = 130
-local Res = Vector(1024,1024)           -- Default = 1024,1024 (This is the highest res you can get)
+local Res = Vector(1024, 1024)           -- Default = 1024,1024 (This is the highest res you can get)
 local AntiAliasing = true               -- Whether to use anti-aliasing in the render
 local Enabled = true                    -- Whether to see the camera by default when Enabled (for owner)
+local ZeroUs = true                     -- Whether to show the disabled screen or not. (If not, the chip will use 0 us when disabled)
 -- Configs --
 
 
@@ -29,8 +35,6 @@ local ScaleMatrix = Matrix()
 ScaleMatrix:setScale(1024/Res)
 
 -- Localized functions for functions that'll be called often
-local math = math
-local render = render
 local selectRenderTarget = render.selectRenderTarget
 local renderViewsLeft = render.renderViewsLeft
 local floor = math.floor
@@ -39,7 +43,7 @@ local curtime = timer.curtime
 local drawTexturedRect = render.drawTexturedRect
 local pushMatrix = render.pushMatrix
 local setRGBA = render.setRGBA
-local drawText = render.drawText
+local drawSimpleText = render.drawSimpleText
 local setMaterial = render.setMaterial
 local renderView = render.renderView
 local isInRenderView = render.isInRenderView
@@ -53,56 +57,73 @@ rtMat:setTextureRenderTarget("$basetexture", "rt")
 
 local RequestingFrames = true -- So we only render the camera when we are looking at the screen
 
-hook.add("renderscene","",function(origin,angles,fov)
-    -- Fps checking
-    if not RequestingFrames then return end
-    RequestingFrames = false
-    if not Enabled then return end
-    if quotaTotalAverage()>quotaMax()*0.5 then return end -- Additional check so that people don't die from quota
+local RViewTbl = {
+    x = 0, y = 0,
+    w = Res.x,
+    h = Res.y,
+    fov = FOV,
+    drawviewer = true
+}
+
+local SLIGHTLY_FORWARD = Vector(10, 0, 0)
+
+local ENT_METHODS = getMethods("Entity")
+local getAngles = ENT_METHODS.getAngles
+local localToWorld = ENT_METHODS.localToWorld
+
+local function renderScene(origin, angles, fov)
+    -- Additional check so that people don't die from quota
     local time = curtime()
     if time < fpstime + fpsdelta then return end
-    deltatime = time-fpstime
+    deltatime = time - fpstime
     fpstime = time
-    
-    if renderViewsLeft()<1 then print("No renderviews left") return end
-    
-    selectRenderTarget("rt")
-        if isInRenderView() then
-            drawText(256,256,"No Infinite Loop Pl0x",1)
-            selectRenderTarget()
-            return
-        end
-        renderView{
-            origin = Chip:localToWorld(Vector(10,0,0)),
-            x = 0,
-            y = 0,
-            w = Res.x,
-            h = Res.y,
-            fov=FOV,
-            angles=Chip:getAngles(),
-            drawviewer = true,
-        }
+
+    selectRenderTarget("rt")        
+        RViewTbl.angles = getAngles( Chip )
+        RViewTbl.origin = localToWorld( Chip, SLIGHTLY_FORWARD )
+        renderView ( RViewTbl )
     selectRenderTarget()
-end)
+end
+
+local function enabledRender()
+    setMaterial(rtMat)
+    pushMatrix(ScaleMatrix)
+        setFilterMag(AntiAliasing and 0 or 1)
+        drawTexturedRect(0, 0, 512, 512)
+    popMatrix()
+
+    drawSimpleText(0, 0, "FPS: " .. min( floor(1 / deltatime), FPS) .. "/" .. FPS )
+end
+
+local disabledRender
+if not ZeroUs then
+    function disabledRender()
+        -- This single function call costs ~120 us :)
+        drawSimpleText(256, 256, "Press E to See RT Camera", 1, 1)
+    end
+end
+
+if Enabled then
+    hook.add("renderscene", "", renderScene)
+    hook.add("render", "", enabledRender)
+elseif not ZeroUs then
+    hook.add("render", "", disabledRender)
+end
 
 hook.add("starfallUsed","",function(ply)
     if localplayer ~= ply then return end
-    Enabled = not Enabled
-end)
 
-hook.add("render","",function()
-    RequestingFrames = true
-    setMaterial(rtMat)
-    pushMatrix(ScaleMatrix)
-    setRGBA(255,255,255,255)
-    setFilterMag(AntiAliasing and 0 or 1)
-    drawTexturedRect(0,0,512,512)
-    popMatrix()
-    if not Enabled then
-        -- It's not in the beginning so we can still see the frame before atleast.
-        setRGBA(255,255,0,255)
-        drawText(256,256,"Press E to See RT Camera",1)
-        return
+    Enabled = not Enabled
+    if Enabled then
+        hook.add("renderscene", "", renderScene) 
+        hook.add("render", "", enabledRender)
+    else
+        hook.remove("renderscene", "")
+        
+        if ZeroUs then
+            hook.remove("render", "")
+        else
+            hook.add("render", "", disabledRender)
+        end
     end
-    drawText(0,0,"FPS: " .. min(floor(1/deltatime),60) .. "/" .. FPS)
 end)
